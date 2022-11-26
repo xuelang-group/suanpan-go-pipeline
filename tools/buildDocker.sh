c81a456317e3809a28557682aa0e1d50b5238396
yanqinghao <woshiyanqinghao@gmail.com>
2022-11-26 16:27:53 +0800

    fix

--- tools/buildDocker.sh
+++ tools/buildDocker.sh
@@ -8,20 +8,20 @@ IMAGE_VERSION="$1"
 
 if [ -n "${PREVIEW}" ]; then
     docker build --pull \
-    -t ${IMAGE_URL}:${PREVIEW}-${VERSION} \
+    -t ${IMAGE_URL}:${PREVIEW}-${IMAGE_VERSION} \
     -t ${IMAGE_URL}:${PREVIEW} \
     . \
     -f ./docker/Dockerfile
 
-    docker push ${IMAGE_URL}:${PREVIEW}-${VERSION}
+    docker push ${IMAGE_URL}:${PREVIEW}-${IMAGE_VERSION}
     docker push ${IMAGE_URL}:${PREVIEW}
 else
     docker build --pull \
-    -t ${IMAGE_URL}:${VERSION} \
+    -t ${IMAGE_URL}:${IMAGE_VERSION} \
     -t ${IMAGE_URL}:latest \
     . \
     -f ./docker/Dockerfile
 
-    docker push ${IMAGE_URL}:${VERSION}
+    docker push ${IMAGE_URL}:${IMAGE_VERSION}
     docker push ${IMAGE_URL}:latest
 fi
