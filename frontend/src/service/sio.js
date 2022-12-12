0499cc7e4481dc49163824afd416a9f11f5bc7c3
liuyu2013 <1347740318@qq.com>
2022-12-12 11:30:39 +0800

    fix

--- frontend/src/service/sio.js
+++ frontend/src/service/sio.js
@@ -22,8 +22,8 @@ class SocketIOError extends Error {
 
 class SocketIOClient {
   constructor() {
-    // this.socket = io('http://10.88.34.184:30080', {path: '/proxr/100026/55149/e3ac66202f5711edb5cbad9c6d3deff4/8888/socket.io', transports: ['websocket'], upgrade: false});
-    this.socket = io({ path: new URL('./socket.io', location.href).pathname, transports: ['websocket'], upgrade: false });
+    this.socket = io('http://10.88.34.184:30080', {path: '/proxr/100026/55182/9d40cc50743d11edad7e29ea3448dc30/8888/socket.io', transports: ['websocket'], upgrade: false});
+    // this.socket = io({ path: new URL('./socket.io', location.href).pathname, transports: ['websocket'], upgrade: false });
     this.timeout = 30 * 1000;
   }
 
