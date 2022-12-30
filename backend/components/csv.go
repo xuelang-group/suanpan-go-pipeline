c8775d80fee01c05a26cd42c26ef055104a71b89
liuyu2013 <1347740318@qq.com>
2022-12-30 13:55:05 +0800

    csvreader

--- backend/components/csv.go
+++ backend/components/csv.go
@@ -29,32 +29,31 @@ func csvDownloaderMain(currentNode Node, inputData RequestData) (map[string]inte
 }
 func CsvFileReaderMain(currentNode Node, inputData RequestData) (map[string]interface{}, error) {
 	//直接传递给下游组件dataframe
-	// args := config.GetArgs()
+	args := config.GetArgs()
 	tmpPath := currentNode.InputData["in1"].(string)
-	// if _, err := os.Stat(tmpPath); errors.Is(err, os.ErrNotExist) {
-	// 	tmpPath = path.Join(args[fmt.Sprintf("--storage-%s-temp-store", args["--storage-type"])], currentNode.InputData["in1"].(string), currentNode.Id, "data.csv")
-	// 	tmpKey := path.Join(currentNode.InputData["in1"].(string), "data.csv")
-	// 	os.MkdirAll(filepath.Dir(tmpPath), os.ModePerm)
-	// 	storageErr := storage.FGetObject(tmpKey, tmpPath)
-	// 	if storageErr != nil {
-	// 		log.Errorf("Can not download file: %s, with error: %s", tmpKey, storageErr.Error())
-	// 		return map[string]interface{}{}, nil
-	// 	}
-	// }
-	// csvFile, err := os.Open(tmpPath)
-	// if err != nil {
-	// 	log.Errorf("Can not open csv file: %s, with error: %s", tmpPath, err.Error())
-	// 	return map[string]interface{}{}, nil
-	// }
-	// defer func() {
-	// 	csvFile.Close()
-	// 	err = os.Remove(tmpPath)
-	// 	if err != nil {
-	// 		log.Errorf("Can not remove csv file: %s, with error: %s", tmpPath, err.Error())
-	// 	}
-	// }()
-	// df := dataframe.ReadCSV(csvFile)
-	// log.Infof("ly---df--%s", df)
-
-	return map[string]interface{}{"out1": tmpPath}, nil
+	if _, err := os.Stat(tmpPath); errors.Is(err, os.ErrNotExist) {
+		tmpPath = path.Join(args[fmt.Sprintf("--storage-%s-temp-store", args["--storage-type"])], currentNode.InputData["in1"].(string), currentNode.Id, "data.csv")
+		tmpKey := path.Join(currentNode.InputData["in1"].(string), "data.csv")
+		os.MkdirAll(filepath.Dir(tmpPath), os.ModePerm)
+		storageErr := storage.FGetObject(tmpKey, tmpPath)
+		if storageErr != nil {
+			log.Errorf("Can not download file: %s, with error: %s", tmpKey, storageErr.Error())
+			return map[string]interface{}{}, nil
+		}
+	}
+	csvFile, err := os.Open(tmpPath)
+	if err != nil {
+		log.Errorf("Can not open csv file: %s, with error: %s", tmpPath, err.Error())
+		return map[string]interface{}{}, nil
+	}
+	defer func() {
+		csvFile.Close()
+		err = os.Remove(tmpPath)
+		if err != nil {
+			log.Errorf("Can not remove csv file: %s, with error: %s", tmpPath, err.Error())
+		}
+	}()
+	df := dataframe.ReadCSV(csvFile)
+	log.Infof("ly---df--%s", df)
+	return map[string]interface{}{"out1": df}, nil
 }
